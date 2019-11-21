a9233a830d83c99498bf15d5805ae1e7e34637c1
laoziwubo <laoziwubo@gmail.com>
2019-11-21 11:40:03 +0800

    基础架子

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import api from './api/http'
+Vue.prototype.$api = api
 
 Vue.config.productionTip = false
 
-new Vue({
+window.vue = new Vue({
   router,
   store,
   render: h => h(App)
